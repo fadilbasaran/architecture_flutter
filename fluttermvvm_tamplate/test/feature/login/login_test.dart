985359ad9b9e414105af23ee5a6fba7d5e1a0cb7
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2022-12-01 20:20:23 +0300

    Dosya Klasörleri yapıldı

---
+++ fluttermvvm_tamplate/test/feature/login/login_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(
+    () {}, //ilk çalıştığınızda buraya gelir
+  );
+  //MARK:Test1
+  test(
+    'User-Login Fail Test',
+    () {
+      const isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    },
+  );
+  //MARK:Test2
+    test(
+    'User-Login Fail Test',
+    () {
+      const isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    },
+  );
+  //MARK:Test3
+    test(
+    'User-Login Fail Test',
+    () {
+      const isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    },
+  );
+  //MARK:Test4
+    test(
+    'User-Login Fail Test',
+    () {
+      const isUserLogin = true;
+      expect(isUserLogin, isTrue);
+    },
+  );
+}
