7a54da4972d4738d52d11770a2a74401d2ab7cf5
fadilbasaran <fadil.basaran.014@gmail.com>
2022-12-02 11:49:11 +0300

    Mobx yapısı oluşturuldu ve viewModel, test_view eklendi

--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttermvm_tamplate/core/base/state/base_state.dart';
 import 'package:fluttermvm_tamplate/core/base/view/base_widget.dart';
+import 'package:fluttermvm_tamplate/view/authenticate/test/viewModel/test_view_model.dart';
 
 class TestView extends StatefulWidget {
   const TestView({super.key});
@@ -12,8 +13,8 @@ class TestView extends StatefulWidget {
 class _TestViewState extends BaseState<TestView> {
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-        viewModel: 'Nerde nerde nerdesin dost',
+    return BaseView<TestViewModel>(
+        viewModel: TestViewModel(),
         onPageBuilder: (context, value) => const Text('data'));
   }
 }
--- lib/view/authenticate/test/viewModel/test_view_model.dart
+++ lib/view/authenticate/test/viewModel/test_view_model.dart
@@ -4,5 +4,6 @@ part 'test_view_model.g.dart';
 class TestViewModel = _TestViewModelBase with _$TestViewModel;
 
 abstract class _TestViewModelBase with Store {
+
   
 }
