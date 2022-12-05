5b5f1ccf4e7ccc1e676ec31799860ef7fb235d91
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2022-12-05 23:49:08 +0300

    Locale eklendi

--- lib/core/init/lang/language_manager.dart
+++ lib/core/init/lang/language_manager.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/material.dart';
+
 class LanguageManager {
   static LanguageManager? _instance;
   static LanguageManager? get instance {
-    _instance ??= LanguageManager();//eğer null ise bunu kullan
+    _instance ??= LanguageManager._init(); //eğer null ise bunu kullan
     return _instance;
   }
+
+  LanguageManager._init();
+
+  final enLocale = const Locale('en', 'US');
+  List<Locale> get supportLocales => [enLocale];
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,13 @@
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermvm_tamplate/core/init/lang/language_manager.dart';
 
 void main() => runApp(EasyLocalization(
     child: const MyApp(),
     
     // ignore: prefer_const_literals_to_create_immutables
-    supportedLocales: [
-      const Locale.fromSubtags(languageCode: 'zh'), // generic Chinese 'zh'
-      const Locale.fromSubtags(
-          languageCode: 'zh',
-          scriptCode: 'Hans'), // generic simplified Chinese 'zh_Hans'
-      const Locale.fromSubtags(
-          languageCode: 'zh',
-          scriptCode: 'Hant'), // generic traditional Chinese 'zh_Hant'
-      const Locale.fromSubtags(
-          languageCode: 'zh',
-          scriptCode: 'Hans',
-          countryCode: 'CN'), // 'zh_Hans_CN'
-      const Locale.fromSubtags(
-          languageCode: 'zh',
-          scriptCode: 'Hant',
-          countryCode: 'TW'), // 'zh_Hant_TW'
-      const Locale.fromSubtags(
-          languageCode: 'zh',
-          scriptCode: 'Hant',
-          countryCode: 'HK'), // 'zh_Hant_HK'
-    ],
+    supportedLocales:LanguageManager.instance!.supportLocales,
     path: ''));
 
 class MyApp extends StatelessWidget {
