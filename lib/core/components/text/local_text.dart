412e91abf366b5aa3805fedbd499e5817c611721
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2022-12-06 21:32:56 +0300

    Easy localization bitti

---
+++ lib/core/components/text/local_text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermvm_tamplate/core/extension/string_extension.dart';
+
+class LocalText extends StatelessWidget {
+  const LocalText({super.key, required this.value});
+  final String value;
+  @override
+  Widget build(BuildContext context) {
+    
+    return Text(value.locale);
+  }
+}
--- lib/view/authenticate/test/view/test_view.dart
+++ lib/view/authenticate/test/view/test_view.dart
@@ -1,8 +1,15 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:fluttermvm_tamplate/core/base/state/base_state.dart';
 import 'package:fluttermvm_tamplate/core/base/view/base_widget.dart';
+import 'package:fluttermvm_tamplate/core/init/lang/language_manager.dart';
+import 'package:fluttermvm_tamplate/core/init/lang/locale_keys.g.dart';
 import 'package:fluttermvm_tamplate/view/authenticate/test/viewModel/test_view_model.dart';
+
+import '../../../../core/extension/string_extension.dart';
 
 class TestView extends StatefulWidget {
   const TestView({super.key});
@@ -24,9 +31,23 @@ class _TestViewState extends BaseState<TestView> {
   }
 
   Widget get scaffoldBody => Scaffold(
+        appBar: AppBar(
+          title: textWelcomeWidget(),
+          actions: [iconButtonChangeTheme()],
+        ),
         floatingActionButton: flutterActionButtomNumberIncrement(),
         body: textNumber,
       );
+
+  Text textWelcomeWidget() => Text(LocaleKeys.welcome.locale);
+
+  IconButton iconButtonChangeTheme() {
+    return IconButton(
+        onPressed: () {
+          context.locale = LanguageManager.instance!.enLocale;
+        },
+        icon: const Icon(Icons.change_history));
+  }
 
   FloatingActionButton flutterActionButtomNumberIncrement() =>
       FloatingActionButton(onPressed: () => viewModel.incrementNumber());
