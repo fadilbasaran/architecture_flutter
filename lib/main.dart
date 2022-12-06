8b35cb6db7a6a160109297a44bbff5f1833c8b80
Fadıl Başaran <59345355+fadilbasaran@users.noreply.github.com>
2022-12-06 20:07:28 +0300

    Klasör Yapısı tekrardan düzenlendi

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttermvm_tamplate/core/constants/app/app_contstants.dart';
 import 'package:fluttermvm_tamplate/core/init/lang/language_manager.dart';
 
 void main() => runApp(EasyLocalization(
     child: const MyApp(),
-    
+
     // ignore: prefer_const_literals_to_create_immutables
-    supportedLocales:LanguageManager.instance!.supportLocales,
-    path: ''));
+    supportedLocales: LanguageManager.instance!.supportLocales,
+    path: ApplicationConstants.LANG_ASSET_PATH));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
